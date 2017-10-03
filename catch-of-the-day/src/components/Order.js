402af249b99b6b89a634037ff45da58ec8c1bd77
Artur Barseghyan <artur.barseghyan@gmail.com>
2017-10-03 16:51:36 +0200

    more

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { formatPrice } from '../helpers';
-
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Order extends React.Component {
     constructor() {
@@ -51,13 +51,18 @@ class Order extends React.Component {
         return (
             <div className="order-wrap">
                 <h2>Your Order</h2>
-                <ul className="order">
+                <CSSTransitionGroup
+                        className="order"
+                        component="ul"
+                        transitionName="order"
+                        transitionEnterTimeout={1000}
+                        transitionLeaveTimeout={1000}>
                     {orderIds.map(this.renderOrder)}
                     <li className="total">
                         <strong>Total:</strong>
                         {formatPrice(total)}
                     </li>
-                </ul>
+                </CSSTransitionGroup>
             </div>
         )
     }
